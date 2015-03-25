9201ac037e6f99753a8d4d03218337c6995eafc2
Nepster <nepsterxxx@mail.ru>
2015-03-25 19:28:16 +0200

    update

---
+++ server.php
@@ -0,0 +1,31 @@
+<?php
+
+if (isset($_GET['term'])) {
+    $result = [];
+    $result[] = [
+        'id' => 1,
+        'name' => "Телевизоры",
+        'desc' => "Категория телевизоров",
+        'icon' => "images/items/1.jpg"
+    ];
+    $result[] = [
+        'id' => 2,
+        'name' => "Кондиционеры",
+        'desc' => "Категория кондиционеров",
+        'icon' => "images/items/2.jpg"
+    ];
+    $result[] = [
+        'id' => 3,
+        'name' => "Стиральные машины",
+        'desc' => "Категория стиральных машин",
+        'icon' => "images/items/3.jpg"
+    ];
+    $result[] = [
+        'id' => 4,
+        'name' => "Компьютеры",
+        'desc' => "Категория компьютеров",
+        'icon' => "images/items/4.jpg"
+    ];
+    echo json_encode($result);
+    exit;
+}
